7a944396076ee666ef985b932ecf6859655b5f69
EvasiveXkiller <evasivexkiller@gmail.com>
2021-08-18 14:58:41 +0800

    0.0.0.0.0000000
    Advanced semantic versioning

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+// no idea how to import these
+
+import axios0 from "axios/lib/adapters/http";
+axios.defaults.adapter = axios0;
+import tough from "tough-cookie";
+import _ from "lodash";
+
+// no idea is this correct
+const querystring = require('querystring')
+
+// you found a kitten, please collect it
+
+
