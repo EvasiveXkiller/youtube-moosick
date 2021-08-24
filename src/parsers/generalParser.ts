1a46c41ae8cf40b525c8d8430084d422cc5e8772
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-08-24 20:43:48 +0800

    Housekeeping

--- src/parsers/generalParser.ts
+++ src/parsers/generalParser.ts
@@ -1,6 +1,6 @@
 import { Category, CategoryURIBase64 } from '../enums';
 import objectScan from 'object-scan';
-import type { Song } from '../resources/generalTypes/song';
+import { Song } from '../resources/generalTypes/song';
 import type {
 	GeneralFull,
 	MusicResponsiveListItemFlexColumnRenderer,
@@ -13,20 +13,18 @@ import { Playlist } from '../resources/generalTypes/playlist';
 import { ArtistExtended } from '../resources/generalTypes/artist';
 import { ParsersExtended } from './parsersExtended';
 import { Results } from '../resources/resultTypes/results';
-
-// TODO: i'm making a lot of assumptions for text being at [0], probably stop
-// TODO: objectScan's syntax is verbose as hell, write abstraction functions
+import { $$ } from '../resources/utilities/objectScan.utility';
 
 export class GeneralParser {
 	// Make this one global function and call the other stuff
 	// Probably other methods should be private
 	static parseSearchResult(context: GeneralFull, searchType?: CategoryURIBase64): Results {
 		// prep all the parts
-		let albums: AlbumExtended[];
-		let videos: Video[];
-		let playlists: Playlist[];
-		let artist: ArtistExtended[];
-		let songs: Song[];
+		const albums: AlbumExtended[] = [];
+		const videos: Video[] = [];
+		const playlists: Playlist[] = [];
+		const artist: ArtistExtended[] = [];
+		const songs: Song[] = [];
 		const musicShelf = objectScan(['**.musicShelfRenderer'], {
 			rtn: 'value',
 			reverse: false,
@@ -37,10 +35,7 @@ export class GeneralParser {
 				reverse: false,
 			})(shelfItem) as MusicResponsiveListItemRenderer[];
 			for (const item of shelfContent) {
-				const flexColumnRenderer = objectScan(['**.musicResponsiveListItemFlexColumnRenderer'], {
-					rtn: 'parent',
-					reverse: false,
-				})(item) as MusicResponsiveListItemFlexColumnRenderer[];
+				const flexColumnRenderer = $$('.musicResponsiveListItemFlexColumnRenderer')(item) as MusicResponsiveListItemFlexColumnRenderer[];
 				const category = flexColumnRenderer[0].text.runs[0].text as Category;
 				switch (category) {
 					case Category.SONG:
@@ -56,7 +51,6 @@ export class GeneralParser {
 							...ParsersExtended.flexSecondRowComplexParser(flexColumnRenderer[1].text.runs, Category.VIDEO, Boolean(searchType)),
 							thumbnails: ParsersExtended.thumbnailParser(item),
 						}));
-						GeneralParser.parseVideoSearchResult(item);
 						break;
 					case Category.PLAYLISTS:
 						playlists.push(Playlist.from({
@@ -65,34 +59,26 @@ export class GeneralParser {
 								reverse: false,
 								abort: true,
 							})(flexColumnRenderer) as string,
-							browseId: flexColumnRenderer.navigationEndpoint.browseEndpoint.browseId,
+							browseId: item.navigationEndpoint?.browseEndpoint?.browseId ?? '',
 							...ParsersExtended.flexSecondRowComplexParser(flexColumnRenderer[1].text.runs, Category.PLAYLISTS, Boolean(searchType)),
 						}));
 						break;
 					case Category.ARTIST:
 						artist.push(ArtistExtended.from({
-							name: objectScan(['**.text'], {
-								rtn: 'value',
-								reverse: false,
-								abort: true,
-							})(flexColumnRenderer),
-							browseId: item.navigationEndpoint.browseEndpoint.browseId,
+							name: flexColumnRenderer[0].text.runs[0].text,
+							browseId: item.navigationEndpoint?.browseEndpoint?.browseId ?? '',
 							thumbnails: ParsersExtended.thumbnailParser(item),
 							...ParsersExtended.flexSecondRowComplexParser(flexColumnRenderer[1].text.runs, Category.ARTIST, Boolean(searchType)),
 						}));
 					// eslint is drunk here
-					// eslint-disable-next-line no-fallthrough
+					// eslint-disable-next-line
 					case Category.ALBUM:
 					case Category.SINGLE:
 					case Category.EP:
 						albums.push({
 							...ParsersExtended.flexSecondRowComplexParser(flexColumnRenderer[1].text.runs, Category.ARTIST, Boolean(searchType)),
-							name: objectScan(['**.text'], {
-								rtn: 'value',
-								reverse: false,
-								abort: true,
-							})(flexColumnRenderer),
-							browseId: item.navigationEndpoint.browseEndpoint.browseId,
+							name: flexColumnRenderer[0].text.runs[0].text,
+							browseId: item.navigationEndpoint?.browseEndpoint?.browseId ?? '',
 						});
 						break;
 					default:
@@ -108,8 +94,7 @@ export class GeneralParser {
 		};
 
 		return Results.from({
-			results: unsorted,
-			continuation: undefined,
+			result: unsorted,
 		});
 	}
 
@@ -117,7 +102,7 @@ export class GeneralParser {
 	 * Only works for video and song
 	 * @param musicResponsiveListItemRenderer
 	 */
-	static musicResponsiveListItemRendererParser(musicResponsiveListItemRenderer: MusicResponsiveListItemRenderer): Partial<Song> {
+	static musicResponsiveListItemRendererParser(musicResponsiveListItemRenderer: MusicResponsiveListItemRenderer) {
 		const display = objectScan(['**.musicResponsiveListItemFlexColumnRenderer'], {
 			rtn: 'value',
 			reverse: false,
