83d14445c740721f6f5d746e5db0eaf6f22720f2
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-08-24 20:45:12 +0800

    Fix signature override

--- src/parsers/parsersExtended.ts
+++ src/parsers/parsersExtended.ts
@@ -1,25 +1,26 @@
 import type { PurpleRun } from '../resources/rawResultTypes/comfirmedInterfaces';
-import { CategoryType, ConstantURLs } from '../enums';
+import { Category, ConstantURLs } from '../enums';
 import { Artist, ArtistExtended } from '../resources/generalTypes/artist';
 import type { Video } from '../resources/generalTypes/video';
 import type { Song } from '../resources/generalTypes/song';
-import { Album } from '../resources/generalTypes/album';
+import { Album, AlbumExtended } from '../resources/generalTypes/album';
 import type { Playlist } from '../resources/generalTypes/playlist';
-import { parseInt } from 'lodash';
 import type { Thumbnails } from '../resources/generalTypes/thumbnails';
 import objectScan from 'object-scan';
 import { utils } from '../utils';
+import { IllegalArgumentError } from '../resources/errors';
 
 export class ParsersExtended {
-	// FIXME how do you overload this????
-	static flexSecondRowComplexParser(runsArray: PurpleRun[], categoryType: CategoryType.ARTIST, trim: boolean): Partial<ArtistExtended>
-	static flexSecondRowComplexParser(runsArray: PurpleRun[], categoryType: CategoryType.VIDEO, trim: boolean): Partial<Video>
-	static flexSecondRowComplexParser(runsArray: PurpleRun[], categoryType: CategoryType.SONG, trim: boolean): Partial<Song>
-	static flexSecondRowComplexParser(runsArray: PurpleRun[], categoryType: CategoryType.ALBUM | CategoryType.SINGLE | CategoryType.EP, trim: boolean): Partial<Album>
-	static flexSecondRowComplexParser(runsArray: PurpleRun[], categoryType: CategoryType.PLAYLISTS, trim: boolean): Partial<Playlist> | undefined { // return array of capable stuff
+	static flexSecondRowComplexParser(runsArray: PurpleRun[], categoryType: Category.ARTIST, trim: boolean): Pick<ArtistExtended, 'subs'>
+	static flexSecondRowComplexParser(runsArray: PurpleRun[], categoryType: Category.VIDEO, trim: boolean): Pick<Video, 'author' | 'views' | 'length'>
+	static flexSecondRowComplexParser(runsArray: PurpleRun[], categoryType: Category.SONG, trim: boolean): Pick<Song, 'artist' | 'album' | 'duration'>
+	static flexSecondRowComplexParser(runsArray: PurpleRun[], categoryType: Category.ALBUM | Category.SINGLE | Category.EP, trim: boolean): Pick<AlbumExtended, 'year'>
+	static flexSecondRowComplexParser(runsArray: PurpleRun[], categoryType: Category.PLAYLISTS, trim: boolean): Pick<Playlist, 'trackCount' | 'author'>
+	static flexSecondRowComplexParser(runsArray: PurpleRun[], categoryType: Category, trim: boolean): any { // return array of capable stuff
 		const delimiter = ' • ';
-		const type = categoryType ?? runsArray[0].text as CategoryType;
-		let artist: Artist[];
+		const type = categoryType ?? runsArray[0].text as Category;
+		let artist: Artist[] = [];
+
 		if (trim) {
 			runsArray.splice(0, 2);
 		}
@@ -30,35 +31,35 @@ export class ParsersExtended {
 		}
 
 		switch (type) {
-			case CategoryType.SONG:
+			case Category.SONG:
 				return {
 					artist,
 					album: this.albumParser(runsArray.slice(positions[0] + 1, positions[1])),
 					duration: utils.hms2ms(runsArray[runsArray.length - 1].text),
 				};
-			case CategoryType.VIDEO:
+			case Category.VIDEO:
 				return {
 					author: artist,
 					views: utils.hms2ms(runsArray[positions[0] + 1].text),
-					length = utils.hms2ms(runsArray[runsArray.length - 1].text),
+					length: utils.hms2ms(runsArray[runsArray.length - 1].text),
 				};
-			case CategoryType.EP:
-			case CategoryType.SINGLE:
-			case CategoryType.ALBUM:
+			case Category.EP:
+			case Category.SINGLE:
+			case Category.ALBUM:
 				return {
-					year: parseInt(runsArray[runsArray.length - 1].text, 10),
+					year: Number(runsArray[runsArray.length - 1].text),
 				};
-			case CategoryType.PLAYLISTS:
+			case Category.PLAYLISTS:
 				return {
-					trackCount: parseInt(runsArray[runsArray.length - 1].text, 10),
+					trackCount: Number(runsArray[runsArray.length - 1].text),
 					author: artist,
 				};
-			case CategoryType.ARTIST:
+			case Category.ARTIST:
 				return {
-					subs: runsArray[runsArray.length - 1].text,
+					subs: Number(runsArray[runsArray.length - 1].text),
 				};
 			default:
-				break;
+				throw new IllegalArgumentError('Unrecognized category', 'categoryType');
 		}
 	}
 
@@ -80,8 +81,8 @@ export class ParsersExtended {
 	static artistParser(artistRaw: PurpleRun[]): Artist[] {
 		return artistRaw.map((artist) => (Artist.from({
 			name: artist.text,
-			browseId: artist.navigationEndpoint.browseEndpoint.browseId,
-			url: ConstantURLs.CHANNEL_URL + artist.navigationEndpoint.browseEndpoint.browseId,
+			browseId: artist.navigationEndpoint?.browseEndpoint?.browseId ?? '',
+			url: ConstantURLs.CHANNEL_URL + String(artist.navigationEndpoint?.browseEndpoint?.browseId ?? ''),
 		})));
 	}
 
@@ -92,8 +93,8 @@ export class ParsersExtended {
 	static albumParser(albumRaw: PurpleRun[]): Album[] { // return the albumObject only
 		return albumRaw.map((album) => (Album.from({
 			name: album.text,
-			browseId: album.navigationEndpoint.browseEndpoint.browseId,
-			url: 'ConstantURLs.CHANNEL_URL' + album.navigationEndpoint.browseEndpoint.browseId,
+			browseId: album.navigationEndpoint?.browseEndpoint?.browseId ?? '',
+			url: ConstantURLs.CHANNEL_URL + String(album.navigationEndpoint?.browseEndpoint?.browseId ?? ''),
 		})));
 	}
 }
