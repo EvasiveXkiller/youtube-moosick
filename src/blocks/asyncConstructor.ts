03164490fa9d341002836bc1dd2b432a043ed0fb
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-08-24 20:44:14 +0800

    Add asyncConstructor base class

---
+++ src/blocks/asyncConstructor.ts
@@ -0,0 +1,23 @@
+import { IllegalInvocationError } from '../resources/errors/illegalInvocation.error';
+
+export class AsyncConstructor {
+	private static isBeingInstantiated = false;
+
+	protected constructor() {
+		if (!(this.constructor as typeof AsyncConstructor).isBeingInstantiated) {
+			throw new IllegalInvocationError('Constructor was called directly. (Use "await [class].new()" instead)');
+		}
+
+		(this.constructor as typeof AsyncConstructor).isBeingInstantiated = false;
+	}
+
+	public static async new<T = AsyncConstructor>(..._args: any[]): Promise<T> {
+		this.brace();
+
+		return undefined as unknown as Promise<T>;
+	}
+
+	protected static brace(): void {
+		this.isBeingInstantiated = true;
+	}
+}
