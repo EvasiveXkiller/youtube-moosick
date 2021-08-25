af8a77f6439114f88cdca1c3e6a2cf5783fecb39
EvasiveXkiller <evasivexkiller@gmail.com>
2021-08-25 15:10:36 +0800

    Basic README.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# youtube-moosick
-moooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+# Youtube-MooSick
+
+<img alt="GitHub Workflow Status" src="https://img.shields.io/github/workflow/status/EvasiveXkiller/youtube-moosick/rollup-build">
+
+> API? ¯\_(ツ)_/¯
+<hr>
+
+## Information
+<hr>
+That's all we have for now...
+
+Star the repo to stay in the loop
+
+## Contributors
+<hr>
+
+- [EvasiveXkiller](https://github.com/EvasiveXkiller)
+
+- [Sxxov](https://github.com/Sxxov)
--- tests/test.mjs
+++ tests/test.mjs
@@ -1,18 +1,53 @@
-"use strict";
+let test = require('tape');
+import {GetArtistParser} from "../dist/parsers/getArtistParser.js"
+// test('timing test', function (t) {
+// 	t.plan(2);
+//
+// 	t.equal(typeof Date.now, 'function');
+// 	let start = Date.now();
+//
+// 	setTimeout(function () {
+// 		t.equal(Date.now() - start, 100);
+// 	}, 100);
+// });
 
-import MooSick from "../dist/index.js";
+test("getArtistPlaylistParser", function (t) {
+
+	const dummyData = require("./dummy/rawGetArtistURL.json")
+
+	const result = GetArtistParser.parseArtistURLPage(dummyData)
+
+	console.log(result);
+
+	t.end();
+})
 
 
-async function test() {
-    const api = new MooSick.MooSick();
-    await api.initialize();
-}
-
-test()
-
-
-
-// * keeps the scripts alive
-setInterval(function () {
-	console.log("timer that keeps nodejs processing running");
-}, 1000 * 60 * 60);
+// let test = require('tape');
+// const {GetArtistParser} = require("../dist/parsers/getArtistParser");
+// const dummyData = require("./dummy/rawGetArtistURL.json");
+//
+// // test('timing test', function (t) {
+// // 	t.plan(2);
+// //
+// // 	t.equal(typeof Date.now, 'function');
+// // 	let start = Date.now();
+// //
+// // 	setTimeout(function () {
+// // 		t.equal(Date.now() - start, 100);
+// // 	}, 100);
+// // });
+//
+// test("getArtistPlaylistParser", function (t) {
+// 	const dummyData = require("./dummy/rawGetArtistURL.json")
+//
+// 	const result = GetArtistParser.parseArtistURLPage(dummyData);
+//
+// 	console.log(result);
+//
+// 	t.end();
+// })
+// // * keeps the scripts alive
+// setInterval(function () {
+// 	console.log("timer that keeps nodejs processing running");
+// }, 1000 * 60 * 60);
