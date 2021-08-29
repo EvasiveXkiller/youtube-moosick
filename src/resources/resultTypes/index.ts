29373f714ffd3a00274bc7db89716d6eb9c819b5
EvasiveXkiller <evasivexkiller@gmail.com>
2021-08-29 14:01:01 +0800

    added `index.ts` to `results`

---
+++ src/resources/resultTypes/index.ts
@@ -0,0 +1,6 @@
+export * from './albumURL.js';
+export * from './artistURL.js';
+export * from './results.js';
+export * from './searchResults.js';
+export * from './searchSuggestions.js';
+export * from './playlistURL.js';
