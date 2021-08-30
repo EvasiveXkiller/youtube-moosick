5d46fe876a4bdccbd6c8312b15caf5d82edf68f3
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-08-30 18:15:58 +0800

    Remove extraneous types

--- src/resources/resultTypes/index.ts
+++ src/resources/resultTypes/index.ts
@@ -1,6 +1,4 @@
 export * from './albumURL.js';
 export * from './artistURL.js';
-export * from './results.js';
-export * from './searchResults.js';
 export * from './searchSuggestions.js';
 export * from './playlistURL.js';
