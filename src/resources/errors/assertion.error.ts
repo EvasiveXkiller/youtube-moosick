6cda38202dd48437bbcc77853e968d8dd21e5490
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-05 04:07:55 +0800

    Apply `AssertionError` constructor args onto proto
    Makes debugging easier

--- src/resources/errors/assertion.error.ts
+++ src/resources/errors/assertion.error.ts
@@ -1,7 +1,7 @@
 import { ClientError } from './client.error.js';
 
 export class AssertionError extends ClientError {
-	constructor(message?: string, item1?: any, item2?: any) {
+	constructor(message?: string, private item1?: any, private item2?: any) {
 		super(`Assertion was false${message ? `: ${message}` : ''}`);
 
 		if (item1 !== undefined
