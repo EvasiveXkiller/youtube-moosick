bf3856223a5066df98f0eba459448c1f0f090fa9
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-05 04:03:59 +0800

    Add `UnsortedFactory` & `ContinuableUnsorted`

--- src/resources/generalTypes/unsorted.ts
+++ src/resources/generalTypes/unsorted.ts
@@ -1,14 +1,29 @@
-import { Item } from '../../blocks/item.js';
-import type { Album } from './album.js';
-import type { Artist } from './artist.js';
+import { Factory } from '../../blocks/item.js';
+import type { AlbumExtended } from './album.js';
+import type { ArtistExtended } from './artist.js';
+import { ContinuableResult } from './continuableResult.js';
 import type { Playlist } from './playlist.js';
 import type { Song } from './song.js';
 import type { Video } from './video.js';
 
-export class Unsorted extends Item {
-	public declare albums: Album[];
+export type UnsortedSubTypes = Video | Song | Playlist | ArtistExtended | AlbumExtended;
+
+export class UnsortedFactory extends Factory<
+	Unsorted,
+	Pick<Unsorted, Exclude<keyof Unsorted, keyof any[]>>
+> {
+	constructor() {
+		super(Unsorted);
+	}
+}
+
+export class Unsorted extends Array<UnsortedSubTypes> {
+	public declare albums: AlbumExtended[];
 	public declare videos: Video[];
 	public declare playlists: Playlist[];
-	public declare artist: Artist[];
+	public declare artist: ArtistExtended[];
 	public declare songs: Song[];
 }
+
+export interface ContinuableUnsorted extends ContinuableResult<UnsortedSubTypes>, Unsorted {}
+export class ContinuableUnsorted extends ContinuableResult<UnsortedSubTypes> {}
