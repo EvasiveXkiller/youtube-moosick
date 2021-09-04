6b4c78abc9f388ab79d34f1b3f5fe749d0dc8804
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-09-05 04:56:32 +0800

    Add "GeneralParser" test

--- src/test/tests/unit/unit_generalSearchParser.ts
+++ src/test/tests/unit/unit_generalSearchParser.ts
@@ -1,28 +1,127 @@
 import test from 'tape';
 import { Category } from '../../../enums.js';
 import { GeneralParser } from '../../../parsers/generalParser.js';
-import { WalkUtility } from '../../../resources/utilities/walk.utility.js';
+import { Album, AlbumExtended } from '../../../resources/generalTypes/album.js';
+import { Artist, ArtistExtended } from '../../../resources/generalTypes/artist.js';
+import { Playlist } from '../../../resources/generalTypes/playlist.js';
+import { Song } from '../../../resources/generalTypes/song.js';
+import { Thumbnails } from '../../../resources/generalTypes/thumbnails.js';
+import { Video } from '../../../resources/generalTypes/video.js';
+import { EitherShape, WalkUtility } from '../../../resources/utilities/walk.utility.js';
 import generalSearchFull from '../../dummy/general/generalSearchFull.json';
 
 test('GeneralParser', async (t) => {
-	t.skip('RESULTS API MAY HAVE BEEN CHANED, SKIPPING FOR NOW');
-	// const result = GeneralParser.parseSearchResult(generalSearchFull as any);
-	// const expected: typeof result = [
-	// 	Album.from({
-	// 		browseId: String(),
-	// 		name: String(),
-	// 		url: String(),
-	// 	}),
-	// ];
+	const { result } = GeneralParser.parseSearchResult(generalSearchFull as any);
 
-	// t.true(
-	// 	WalkUtility
-	// 		.walkAndAssertShape(
-	// 			result,
-	// 			expected,
-	// 		),
-	// 	'result has expected shape',
-	// );
+	const expected = [
+		new EitherShape([
+			Video.from({
+				name: String(),
+				url: String(),
+				thumbnails: [
+					Thumbnails.from({
+						height: Number(),
+						url: String(),
+						width: Number(),
+					}),
+				],
+				videoId: String(),
+				author: [
+					Artist.from({
+						name: String(),
+						url: String(),
+						browseId: String(),
+					}),
+				],
+				views: Number(),
+				length: Number(),
+			}),
+			Song.from({
+				name: String(),
+				artist: [
+					Artist.from({
+						browseId: String(),
+						name: String(),
+						url: String(),
+					}),
+				],
+				playlistId: new EitherShape([String(), undefined]) as unknown as string,
+				thumbnails: [
+					Thumbnails.from({
+						height: Number(),
+						url: String(),
+						width: Number(),
+					}),
+				],
+				videoId: String(),
+				url: String(),
+				duration: Number(),
+				album: [
+					Album.from({
+						browseId: String(),
+						name: String(),
+						url: String(),
+					}),
+				],
+				params: String(),
+				type: Category.SONG,
+			}),
+			AlbumExtended.from({
+				name: String(),
+				url: String(),
+				browseId: String(),
+				thumbnails: [
+					Thumbnails.from({
+						height: Number(),
+						url: String(),
+						width: Number(),
+					}),
+				],
+				artist: [
+					Artist.from({
+						name: String(),
+						url: String(),
+						browseId: String(),
+					}),
+				],
+				year: new EitherShape([Number(), undefined]) as unknown as number,
+			}),
+			Playlist.from({
+				name: String(),
+				browseId: String(),
+				author: [
+					Artist.from({
+						name: String(),
+						url: String(),
+						browseId: String(),
+					}),
+				],
+				trackCount: Number(),
+			}),
+			ArtistExtended.from({
+				name: String(),
+				url: String(),
+				browseId: String(),
+				thumbnails: [
+					Thumbnails.from({
+						height: Number(),
+						url: String(),
+						width: Number(),
+					}),
+				],
+				subs: new EitherShape([String(), undefined]) as unknown as string,
+			}),
+		]),
+	];
+
+	t.true(
+		WalkUtility
+			.walkAndAssertShape(
+				result,
+				expected as unknown as typeof result,
+			),
+		'result has expected shape',
+	);
 
 	t.end();
 });
