f150e01638bc650d2097fc7921a09241ecae3e8c
EvasiveXkiller <evasivexkiller@gmail.com>
2021-09-26 22:29:06 +0800

    chore:updateDeps

--- dist/cjs/resources/generalTypes/playlist.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Playlist = void 0;
-const item_js_1 = require("../../blocks/item.js");
-class Playlist extends item_js_1.Item {
-}
-exports.Playlist = Playlist;
-//# sourceMappingURL=playlist.js.map
